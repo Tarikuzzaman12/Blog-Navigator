ccc66fce974167a8df7ca24707d0eb11b5f6a8d7
Tarikuzzaman12 <tarikuzzamantuhin920@gmail.com>
2025-01-24 09:23:22 +0600

    some changes in Navber

--- Components/Navber.jsx
+++ Components/Navber.jsx
@@ -8,8 +8,8 @@ export default function Navbar() {
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         {/* Website Name on the Left */}
-        <div className="text-lg font-bold">
-          <Link href="/">
+        <div>
+          <Link  className="text-lg font-bold" href="/">
             <p>Blog Navigator</p>
           </Link>
         </div>
